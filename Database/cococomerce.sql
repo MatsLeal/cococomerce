6bac788bf1af9d742534ce2f084a9fd46812bbed
Mats Leal <mats.romanoff@gmail.com>
2017-11-10 09:32:07 -0600

    Script y diagrama para base de datos

---
+++ Database/cococomerce.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 10-11-2017 a las 02:10:37
+-- Versión del servidor: 5.7.19
+-- Versión de PHP: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cocomarket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `donaciones`
+--
+
+DROP TABLE IF EXISTS `donaciones`;
+CREATE TABLE IF NOT EXISTS `donaciones` (
+  `id_donador` int(11) NOT NULL,
+  `id_beneficiado` int(11) NOT NULL,
+  `hora_donacion` datetime NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_donacion` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_donacion`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensaje`
+--
+
+DROP TABLE IF EXISTS `mensaje`;
+CREATE TABLE IF NOT EXISTS `mensaje` (
+  `id_mensaje` int(11) NOT NULL AUTO_INCREMENT,
+  `id_donante` int(11) NOT NULL,
+  `id_solicitante` int(11) NOT NULL,
+  `mensaje` text NOT NULL,
+  PRIMARY KEY (`id_mensaje`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `descripcion` text NOT NULL,
+  `indiceimagen` longblob NOT NULL,
+  `bloqueada` tinyint(1) NOT NULL,
+  `fecha_update_producto` datetime NOT NULL,
+  `usuario_ofertor` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos_bloqueados`
+--
+
+DROP TABLE IF EXISTS `productos_bloqueados`;
+CREATE TABLE IF NOT EXISTS `productos_bloqueados` (
+  `id_producto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `descripcion_bloqueo` text NOT NULL,
+  `fecha_bloqueo` datetime NOT NULL,
+  `id_bloqueo` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_bloqueo`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto_pedido`
+--
+
+DROP TABLE IF EXISTS `producto_pedido`;
+CREATE TABLE IF NOT EXISTS `producto_pedido` (
+  `id_pedido` int(11) NOT NULL AUTO_INCREMENT,
+  `id_donante` int(11) NOT NULL,
+  `id_solicitante` int(11) NOT NULL,
+  `ganador` tinyint(1) NOT NULL,
+  `fecha_pedido` datetime NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  PRIMARY KEY (`id_pedido`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(20) NOT NULL,
+  `clave` varchar(20) NOT NULL,
+  `correo` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `correo` (`correo`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
